7cec1a63991d7ee5705d67cce0ec44e5328fbf75
4shutoshbhardwaj <90901694+4shutoshbhardwaj@users.noreply.github.com>
2022-10-29 00:42:15 +0530

    fifth commit

--- client/src/components/home/Banner.jsx
+++ client/src/components/home/Banner.jsx
@@ -4,21 +4,23 @@ import { bannerData } from '../../constants/data';
 import {Box, styled, Typography} from '@mui/material';
 import 'react-multi-carousel/lib/styles.css';
 
-const Image=styled('img')({
-    width:'100%',
-    height:280
-})
+const Image = styled('img')(({ theme }) => ({
+  width: '100%',
+  height: 280,
+  [theme.breakpoints.down('sm')]: {
+      objectFit: 'cover',
+      height: 180
+  }
+}));
 
 const responsive = {
     desktop: {
       breakpoint: { max: 3000, min: 1024 },
-      items: 1,
-      slidesToSlide: 3 // optional, default to 1.
+      items: 1
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 1,
-      slidesToSlide: 2 // optional, default to 1.
+      items: 1
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
--- server/routes/route.js
+++ server/routes/route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { userSingup,userLogin } from "../controller/user-controller.js";
+import { getProducts } from "../controller/product-controller.js";
 
 const router=express.Router();
 
 router.post("/signup",userSingup);
 router.post("/login",userLogin);
-// router.get("/products",getProducts);
+router.get("/products",getProducts);
 
 export default router;
